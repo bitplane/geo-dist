9b607ecbae65b200d0999d171b7b6cb9aee4636f
Gareth Davidson <gaz@bitplane.net>
2023-09-01 11:34:21 +0100

    NN stuff

--- geo-dist-prep/src/geo_dist_prep/normalize.py
+++ geo-dist-prep/src/geo_dist_prep/normalize.py
@@ -9,3 +9,13 @@ def normalize_coords(lat: float, lon: float) -> tuple[float, float]:
     x = ((lon - 150) % 360.0) / 360.0
 
     return y, x
+
+
+def denormalize_coords(y: float, x: float) -> tuple[float, float]:
+    """
+    Denormalize a latitude/longitude pair from [0, 1] to [-90, 90], [-180, 180].
+    """
+    lat = y * 180.0 + 90
+    lon = x * 360.0 + 150
+
+    return lat, lon
