23ff36569ba19ea5d74df4eaff7736b31816a238
Gareth Davidson <gaz@bitplane.net>
2023-09-02 01:20:48 +0100

    use a more stable log message

--- build/docker-up.sh
+++ build/docker-up.sh
@@ -9,8 +9,8 @@ export UID=$UID
 export GID=$GID
 
 docker-compose --file ".cache/ors/$1/docker-compose.yml" up | while read -r line; do
-  echo "$line"  # Optionally print each line
-  if [[ "$line" == *"[routing.RoutingProfileManager] - ====> Memory usage by profiles"* ]]; then
+  echo "$line"
+  if [[ "$line" == *"[routing.RoutingProfile] - [1] Finished at:"* ]]; then
     break
   fi
 done
