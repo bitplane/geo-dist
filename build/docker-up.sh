fc2a3c69872f45e842c8b99c6ef8a85f16a96049
Gareth Davidson <gaz@bitplane.net>
2025-07-23 00:46:09 +0100

    docker run script

--- build/docker-up.sh
+++ build/docker-up.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 # for some reason Python won't pass these variables
+# UID is readonly in bash, so we unset it first
 
+USER_ID=$(id -u)
+GROUP_ID=$(id -g)
 
-UID=$(id -u)
-GID=$(id -g)
-export UID=$UID
-export GID=$GID
-
-docker-compose --file ".cache/ors/$1/docker-compose.yml" up | while read -r line; do
+env -u UID UID="$USER_ID" GID="$GROUP_ID" docker-compose --file ".cache/ors/$1/docker-compose.yml" up | while read -r line; do
   echo "$line"
-  if [[ "$line" == *"[routing.RoutingProfile] - [1] Finished at:"* ]]; then
+  if [[ "$line" == *"] Finished at:"* ]]; then
     break
   fi
 done
